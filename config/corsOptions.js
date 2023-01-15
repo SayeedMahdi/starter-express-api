4222c79e4501e04101fdbd14a480e47fe50a6906
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2023-01-15 08:34:11 +0430

    added config text

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
 	"http://localhost:3000",
 	"http://localhost:3001",
+	"http://192.168.158.204:3000",
 	"http://localhost:3002",
 	"https://rahanet.netlify.app",
 	"https://rahanetisp.netlify.app"
